9b0dd0792d275d499747446bbc8d6849f9497e94
AtomThin <bluefireray@hotmail.com>
2018-05-01 20:45:36 +0800

    Problem#821 done

---
+++ LeetCode.Core/Problems/821-Shortest-Distance-to-a-Character/Algorithms.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace LeetCode.Core.Problems
+{
+    public partial class Algorithms
+    {
+        public int[] ShortestToChar(string S, char C)
+        {
+            var arr = new int[S.Length];
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == C)
+                {
+                    arr[i] = 0;
+                    continue;
+                }
+                else
+                {
+                    var partStr1 = S.Substring(0, i + 1);
+                    var partStr2 = S.Substring(i, S.Length - i);
+                    var index1 = partStr1.Reverse().ToList().FindIndex(m => m == C);
+                    var index2 = partStr2.IndexOf(C);
+                    if (index1 > 0 && index2 > 0)
+                    {
+                        arr[i] = index1 < index2 ? index1 : index2;
+                    }
+                    else
+                    {
+                        if (index1 > 0)
+                        {
+                            arr[i] = index1;
+                        }
+                        if (index2 > 0)
+                        {
+                            arr[i] = index2;
+                        }
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
--- LeetCode.Test/UnitTest.cs
+++ LeetCode.Test/UnitTest.cs
@@ -5,12 +5,20 @@ namespace LeetCode.Test
 {
     public class UnitTest
     {
+        Algorithms al = new Algorithms();
         [Fact]
         public void TwoSum()
         {
-            var al = new Algorithms();
+
             Assert.Equal(new int[] { 0, 1 }, al.TwoSum(new int[] { 2, 7, 11, 15 }, 9));
             Assert.NotEqual(new int[] { 1, 3 }, al.TwoSum(new int[] { 2, 7, 11, 15 }, 18));
+        }
+
+        [Fact]
+        public void ShortestToChar()
+        {
+            Assert.Equal(new int[] { 3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0 }, al.ShortestToChar("loveleetcode", 'e'));
+            Assert.Equal(new int[] { 2, 1, 0, 0, 1 }, al.ShortestToChar("skeet", 'e'));
         }
     }
 }
