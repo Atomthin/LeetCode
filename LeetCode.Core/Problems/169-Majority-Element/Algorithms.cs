04293645520e468930d21213f9dc55f94e3e00de
haiyang <LIXH2@legal.regn.net>
2018-05-24 16:00:43 +0800

    169 done

---
+++ LeetCode.Core/Problems/169-Majority-Element/Algorithms.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Core.Problems
+{
+    public partial class Algorithms
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var majorNum = 0;
+            var count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (count == 0)
+                {
+                    majorNum = nums[i];
+                    count++;
+                }
+                else
+                {
+                    if (majorNum == nums[i])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count--;
+                    }
+                }
+            }
+            return majorNum;
+        }
+    }
+}
