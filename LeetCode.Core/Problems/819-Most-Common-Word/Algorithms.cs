80c551cbd11f7e42c827186484fd098ee34f310d
haiyang <LIXH2@legal.regn.net>
2018-05-25 15:24:08 +0800

    819 done

---
+++ LeetCode.Core/Problems/819-Most-Common-Word/Algorithms.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCode.Core.Problems
+{
+    public partial class Algorithms
+    {
+        public string MostCommonWord(string paragraph, string[] banned)
+        {
+            var count = 0;
+            var temp = string.Empty;
+            var mostCount = 0;
+            var mostWord = string.Empty;
+            var stringArr = paragraph.ToLower().Replace('!', ' ').Replace('?', ' ').Replace('\'', ' ').Replace(',', ' ').Replace(';', ' ').Replace('.', ' ').Split(' ');
+            for (int i = 0; i < stringArr.Length; i++)
+            {
+                if (string.IsNullOrEmpty(stringArr[i]))
+                {
+                    continue;
+                }
+                if (IsExistInBanned(banned, stringArr[i]))
+                {
+                    count = 0;
+                    continue;
+                }
+                if (count == 0)
+                {
+                    temp = stringArr[i];
+                    count++;
+                    for (int j = i + 1; j < stringArr.Length; j++)
+                    {
+                        if (IsExistInBanned(banned, stringArr[j]) || stringArr[j] == mostWord)
+                        {
+                            continue;
+                        }
+                        if (temp == stringArr[j])
+                        {
+                            count++;
+                        }
+                    }
+                }
+                if (count > mostCount)
+                {
+                    mostCount = count;
+                    mostWord = temp;
+                }
+                count = 0;
+            }
+            return mostWord;
+        }
+
+        private bool IsExistInBanned(string[] banned, string word)
+        {
+            for (int i = 0; i < banned.Length; i++)
+            {
+                if (banned[i] == word)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- LeetCode.Test/UnitTest.cs
+++ LeetCode.Test/UnitTest.cs
@@ -220,6 +220,12 @@ namespace LeetCode.Test
             Assert.Equal(new List<IList<int>> { new List<int> { 3, 6 } }, al.LargeGroupPositions("abbxxxxzzy"));
             Assert.Equal(new List<IList<int>> { new List<int> { 0, 2 } }, al.LargeGroupPositions("aaa"));
         }
+
+        [Fact]
+        public void MostCommonWord()
+        {
+            Assert.Equal("ball", al.MostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", new string[] { "hit" }));
+        }
         #endregion
     }
 }
