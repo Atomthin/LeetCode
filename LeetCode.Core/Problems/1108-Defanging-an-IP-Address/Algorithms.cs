d588708485c57fd7a3fd5a6783fbafe5d1f2c4f3
Atomthin <bluefireray@hotmail.com>
2019-07-31 00:16:39 +0800

    “1108 done

---
+++ LeetCode.Core/Problems/1108-Defanging-an-IP-Address/Algorithms.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace LeetCode.Core.Problems
+{
+    public partial class Algorithms
+    {
+        public string DefangIPaddr(string address)
+        {
+            var sb = new StringBuilder();
+            foreach (var item in address)
+            {
+                if (item != '.')
+                {
+                    sb.Append(item);
+                }
+                else
+                {
+                    sb.Append("[.]");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
