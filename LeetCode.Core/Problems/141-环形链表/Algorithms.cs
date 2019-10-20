8456538820426c0f96d3551d60eadb63e6aff10a
Atomthin <bluefireray@hotmail.com>
2019-10-20 19:43:44 +0800

    141,142,160，链表双指针法练习

---
+++ LeetCode.Core/Problems/141-环形链表/Algorithms.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Core.Common;
+
+namespace LeetCode.Core.Problems
+{
+    public partial class Algorithms
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head?.next == null)
+            {
+                return false;
+            }
+            ListNode slowNode = head;
+            ListNode fastNode = head.next;
+            while (slowNode != fastNode)
+            {
+                if (fastNode?.next == null)
+                {
+                    return false;
+                }
+                slowNode = slowNode.next;
+                fastNode = fastNode.next.next;
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode.Core/Problems/142-环形链表 II/Algorithms.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Core.Common;
+
+namespace LeetCode.Core.Problems
+{
+    public partial class Algorithms
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            if (head?.next == null)
+            {
+                return null;
+            }
+            ListNode slowNode = head;
+            ListNode fastNode = head;
+            ListNode cross = null;
+            while (slowNode != null && fastNode?.next != null)
+            {
+                slowNode = slowNode.next;
+                fastNode = fastNode.next.next;
+                if (slowNode == fastNode)
+                {
+                    cross = fastNode;
+                    break;
+                }
+            }
+
+            if (cross == null)
+            {
+                return null;
+            }
+
+            ListNode start = head;
+            while (start != cross)
+            {
+                start = start.next;
+                cross = cross.next;
+            }
+            return cross;
+        }
+    }
+}
