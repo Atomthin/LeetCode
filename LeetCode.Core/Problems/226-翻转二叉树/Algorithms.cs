94ecd6500321c94d60754b6758bfe8fcdb7ffd84
Atomthin <bluefireray@hotmail.com>
2020-01-03 21:56:02 +0800

    226-翻转二叉树 递归完成 执行用时 :112 ms, 在所有 C# 提交中击败了 58.16% 的用户 内存消耗 :23.4 MB, 在所有 C# 提交中击败了 9.52% 的用户

---
+++ LeetCode.Core/Problems/226-翻转二叉树/Algorithms.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LeetCode.Core.Common;
+
+namespace LeetCode.Core.Problems
+{
+    public partial class Algorithms
+    {
+        /// <summary>
+        /// 运用递归完成，注意递归边界条件
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+            var tempNode = root.left;
+            root.left = root.right;
+            root.right = tempNode;
+            root.left = InvertTree(root.left);
+            root.right = InvertTree(root.right);
+            return root;
+        }
+    }
+}
