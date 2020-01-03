fadf3fa3b9542d876aa03d687c360d9e46980382
Atomthin <bluefireray@hotmail.com>
2020-01-03 22:45:14 +0800

    739-每日温度 暴力双循环超时，但是应该可以解决问题，最后运用栈来解决 用栈记录索引，满足 T[stack[-1]] < T[stack[-2]] < ...，其中 stack[-1] 是栈的顶部，stack[-2] 是从顶部开始的第二个元素，依此类推；将在处理每个 T[i] 时保持 stack[-1] > stack[-2] > ...。 通过当前温度和栈顶索引所代表的温度比较来找到温度升高的位置。 举个例子：反向遍历处理 t=[73，74，75，71，69，72，76，73] ，通过看栈元素的变化来理解是如何工作的。为了清楚 stack 只包含索引 i，但是将把 T[i] 的值写在旁边的括号中，例如 0 (73)。 当 i = 7，stack = [7 (73)]。ans[i] = 0。 当 i = 6，stack = [6 (76)]。ans[i] = 0。 当 i = 5，stack = [5 (72), 6 (76)]。ans[i] = 1。 当 i = 4，stack = [4 (69), 5 (72), 6 (76)]。ans[i] = 1。 当 i = 3，stack = [3 (71), 5 (72), 6 (76)]。ans[i] = 2。 当 i = 2，stack = [2 (75), 6 (76)]。ans[i] = 4。 当 i = 1，stack = [1 (74), 2 (75), 6 (76)]。ans[i] = 1。 当 i = 0，stack = [0 (73), 1 (74), 2 (75), 6 (76)]。ans[i] = 1。

---
+++ LeetCode.Core/Problems/739-每日温度/Algorithms.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Core.Problems
+{
+    public partial class Algorithms
+    {
+        /// <summary>
+        /// 注释中的是暴力解法，可以解出来，但是会非常耗时
+        /// 运用栈解决问题，通过当前温度和栈顶索引所代表的温度比较来找到温度升高的位置
+        /// </summary>
+        /// <param name="T"></param>
+        /// <returns></returns>
+        public int[] DailyTemperatures(int[] T)
+        {
+            var resultArr = new int[T.Length];
+            //for (int i = 0; i < T.Length; i++)
+            //{
+            //    var days = 0;
+            //    for (int j = i + 1; j < T.Length; j++)
+            //    {
+            //        if (T[j] > T[i])
+            //        {
+            //            days = j - i;
+            //            break;
+            //        }
+            //    }
+            //    resultArr[i] = days;
+            //}
+            //return resultArr;
+            Stack<int> stack = new Stack<int>();
+            for (int i = T.Length - 1; i >= 0; --i)
+            {
+                while (stack.Count > 0 && T[i] >= T[stack.Peek()]) stack.Pop();
+                resultArr[i] = stack.Count == 0 ? 0 : stack.Peek() - i;
+                stack.Push(i);
+            }
+
+            return resultArr;
+        }
+    }
+}
